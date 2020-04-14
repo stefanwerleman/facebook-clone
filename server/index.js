87affe644f29cc0e42457c65bd4da8e9c4868d4b
stefanwerleman <stefanwerleman@knights.ucf.edu>
2020-04-14 19:06:22 -0400

    Set up GET request

--- server/index.js
+++ server/index.js
@@ -2,6 +2,25 @@ const express = require("express");
 
 const app = express();
 
+// GET Request:
+app.get("/", function(req, res)
+{
+   // TODO: Should load all posts
+   // TODO: Do it in streams for efficiency
+
+   res.json(
+   {
+      username: "@stefanwerleman",
+      message: "This is a dummy post 😝"
+   });
+});
+
+// Post request for new posts
+app.post("/post", function(req, res)
+{
+   console.log(res.body);
+});
+
 app.listen(5000, function()
 {
    console.log("Listening on http://localhost:5000");
